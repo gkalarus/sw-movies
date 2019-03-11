d85f06093b84932ac997f58b5eab7eed5e211187
Grzegorz <kalarus.g@gmail.com>
2019-03-11 08:32:58 +0100

    Updates

---
+++ precache-manifest.3b0618f86f441a661637a9602316e1c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cacb26afc4e101d09a6",
+    "url": "/sw-movies/static/css/main.fd1a87f6.chunk.css"
+  },
+  {
+    "revision": "9cacb26afc4e101d09a6",
+    "url": "/sw-movies/static/js/main.9cacb26a.chunk.js"
+  },
+  {
+    "revision": "a1a9de24449cc29211a6",
+    "url": "/sw-movies/static/js/runtime~main.a1a9de24.js"
+  },
+  {
+    "revision": "88da4ac213324622dfc6",
+    "url": "/sw-movies/static/css/2.48100370.chunk.css"
+  },
+  {
+    "revision": "88da4ac213324622dfc6",
+    "url": "/sw-movies/static/js/2.88da4ac2.chunk.js"
+  },
+  {
+    "revision": "4d66597d8badf6a02257a0704fa622fa",
+    "url": "/sw-movies/static/media/ARROWOPEN.4d66597d.svg"
+  },
+  {
+    "revision": "9f3a719add9d3b8ad4f8d2918acb5059",
+    "url": "/sw-movies/static/media/ARROWCLOSE.9f3a719a.svg"
+  },
+  {
+    "revision": "8b6d494529d2a18c9a45450ed663dce9",
+    "url": "/sw-movies/static/media/LOGO.8b6d4945.svg"
+  },
+  {
+    "revision": "4b823e924575020f741e171a79ab181d",
+    "url": "/sw-movies/static/media/deletePlanet.4b823e92.svg"
+  },
+  {
+    "revision": "c1c442c7f56ffad33b0fce926ae9273a",
+    "url": "/sw-movies/index.html"
+  }
+];
